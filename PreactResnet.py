a693c4a41dff300961edcedc66acbf4ae037f87f
cnguyen10 <cuong.c.nguyen@hotmail.com>
2023-11-01 09:57:52 +1030

    Correct pre-act block

--- PreactResnet.py
+++ PreactResnet.py
@@ -1,5 +1,3 @@
-import jax
-import flax
 from flax import linen as nn
 
 from functools import partial
@@ -8,8 +6,7 @@ from typing import Any, Sequence, Optional
 import chex
 
 
-class PreActBlock(nn.Module):
-    """Pre-activation version of the Basic Block"""
+class BasicBlock(nn.Module):
     in_planes: int
     planes: int
     stride: int = 1
@@ -35,6 +32,39 @@ class PreActBlock(nn.Module):
         out = out + shortcut
 
         out = nn.relu(x=out)
+
+        return out
+
+
+class PreActBlock(nn.Module):
+    """Pre-activation version of the Basic Block"""
+    in_planes: int
+    planes: int
+    stride: int = 1
+    expansion: int = 1
+    train: Optional[bool] = None
+
+    @nn.compact
+    def __call__(self, x: chex.Array, train: Optional[bool] = None) -> Any:
+        train = nn.merge_param(name='train', a=self.train, b=train)
+
+        out = nn.BatchNorm(use_running_average=not train)(x=x)
+        out = nn.relu(x=out)
+
+        out = nn.Conv(features=self.planes, kernel_size=(3, 3), strides=self.stride, padding=1, use_bias=False)(inputs=x)
+        out = nn.BatchNorm(use_running_average=not train)(x=out)
+        out = nn.relu(x=out)
+
+        out = nn.Conv(features=self.planes, kernel_size=(3, 3), strides=1, padding=1, use_bias=False)(inputs=out)
+
+        if self.stride != 1 or self.in_planes != (self.expansion * self.planes):
+            shortcut = nn.BatchNorm(use_running_average=not train)(x=x)
+            shortcut = nn.relu(x=shortcut)
+            shortcut = nn.Conv(features=self.expansion * self.planes, kernel_size=(1, 1), strides=self.stride, use_bias=False)(inputs=shortcut)
+        else:
+            shortcut = x
+
+        out = out + shortcut
 
         return out
 
@@ -86,8 +116,6 @@ class PreActResNetFeature(nn.Module):
         train = nn.merge_param(name='train', a=self.train, b=train)
 
         out = nn.Conv(features=64, kernel_size=(3, 3), strides=1, padding=1, use_bias=False)(inputs=x)
-        out = nn.BatchNorm(use_running_average=not train)(x=out)
-        out = nn.relu(x=out)
 
         in_planes, layer1 = make_layer(block=self.block, in_planes=self.in_planes, planes=64, num_blocks=self.num_blocks[0], stride=1, train=train)
         in_planes, layer2 = make_layer(block=self.block, in_planes=in_planes, planes=128, num_blocks=self.num_blocks[1], stride=2, train=train)
@@ -99,8 +127,7 @@ class PreActResNetFeature(nn.Module):
         out = layer3(out)
         out = layer4(out)
 
-        out = nn.avg_pool(inputs=out, window_shape=(4, 4), strides=(4, 4))
-        out = out.reshape((out.shape[0], -1))
+        out = out.mean(axis=(1, 2))
 
         return out
 
@@ -120,26 +147,6 @@ class PreActResNet(nn.Module):
     num_classes: int = 10
     in_planes: int = 64
 
-    # @nn.compact
-    # def __call__(self, x: chex.Array, train: Optional[bool] = None) -> chex.Array:
-    #     out = nn.Conv(features=64, kernel_size=(3, 3), strides=1, padding=1, use_bias=False)(inputs=x)
-
-    #     in_planes, layer1 = make_layer(block=self.block, in_planes=self.in_planes, planes=64, num_blocks=self.num_blocks[0], stride=1, train=train)
-    #     in_planes, layer2 = make_layer(block=self.block, in_planes=in_planes, planes=128, num_blocks=self.num_blocks[1], stride=2, train=train)
-    #     in_planes, layer3 = make_layer(block=self.block, in_planes=in_planes, planes=256, num_blocks=self.num_blocks[2], stride=2, train=train)
-    #     _, layer4 = make_layer(block=self.block, in_planes=in_planes, planes=512, num_blocks=self.num_blocks[3], stride=2, train=train)
-
-    #     out = layer1(out)
-    #     out = layer2(out)
-    #     out = layer3(out)
-    #     out = layer4(out)
-
-    #     out = nn.avg_pool(inputs=out, window_shape=(4, 4), strides=(4, 4))
-    #     out = out.reshape((out.shape[0], -1))
-    #     out = nn.Dense(features=self.num_classes)(inputs=out)
-
-    #     return out
-
     def setup(self) -> None:
         self.features = PreActResNetFeature(block=self.block, num_blocks=self.num_blocks, in_planes=self.in_planes)
         self.classifier = Classifier(num_classes=self.num_classes)
@@ -151,8 +158,12 @@ class PreActResNet(nn.Module):
         return out
 
 
+def ResNet(num_blocks: Sequence[int], num_classes: int) -> PreActResNet:
+    return PreActResNet(block=PreActBlock, num_blocks=num_blocks, num_classes=num_classes)
+
+
 def ResNet18(num_classes: int = 10) -> PreActResNet:
-    return PreActResNet(block=PreActBlock, num_blocks=(2, 2, 2, 2), num_classes=num_classes)
+    return ResNet(num_blocks=(2, 2, 2, 2), num_classes=num_classes)
 
 
 def make_layer(block: PreActBlock | PreActBottleneck, in_planes: int, planes: int, num_blocks: Sequence[int], stride: int, train: bool) -> tuple[int, nn.Module]:
